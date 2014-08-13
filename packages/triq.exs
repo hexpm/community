57bee939075ca7a97eac03a3316aad1245b72538
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-13 03:20:34 +0200

    triq: sync ver with app.src

--- packages/triq.exs
+++ packages/triq.exs
@@ -3,7 +3,7 @@ defmodule Triq.Mixfile do
 
   def project do
     [app: :triq,
-      version: "0.0.1",
+      version: "0.1.0",
       description: description,
       package: package,
       deps: [],
